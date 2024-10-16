e92fa504a9aff034e43e94c4049fc40e8d027d52
Marcelo S Carvalho <marceloscarvalho100@gmail.com>
2024-10-16 13:58:38 -0300

    raiz quadrada

--- math.js
+++ math.js
@@ -1,5 +1,20 @@
 function add(a, b) {
   return a + b;
 }
+function subtract(a, b) {
+  return a - b;
+}
+function multiply(a, b) {
+  return a * b;
+}
+function divide(a, b) {
+  if (b === 0) {
+    throw new Error("Division by zero is not allowed");
+  }
+  return a / b;
+}
 
-export { add };
+function square(a) {
+  return a ** 2;
+}
+export { add, divide };
