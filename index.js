ede7ce1c862ce8fd0f4c5ef588de1840df935aed
Marcelo S Carvalho <marceloscarvalho100@gmail.com>
2024-10-16 14:05:35 -0300

    função multiplicação

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-consonle.log("hello world!");
+import { add, divide } from "./math.js";
+
+console.log(add(2, 3)); // Outputs: 5
+console.log("hello world!");
+
+try {
+  const e = divide(10, 7); // Outputs: 5
+  console.log(`divisão: ${e}`);
+} catch (error) {
+  console.error("Error:", error.message);
+}
+
+const m = multiply(10, 7); // Outputs:
+console.log(`multiplicação: ${e})`);
