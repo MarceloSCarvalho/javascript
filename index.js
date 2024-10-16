51148994b1e7b43b58d54a88130dccc1c8983228
Marcelo S Carvalho <marceloscarvalho100@gmail.com>
2024-10-16 14:58:02 -0300

    todas as funções

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-import { add, subtract } from "./math.js";
+import { add, subtract, divide, multiply } from "./math.js";
 
 console.log(add(5, 3)); // Output: 8
+
+console.log(subtract(5, 3)); // Output:)
+
+console.log(divide(5, 3)); // Output: 1.6666666666666667'
+
+console.log(multiply(5, 3)); // Output: 15
+
+console.log("Hello, world!"); // Output:
